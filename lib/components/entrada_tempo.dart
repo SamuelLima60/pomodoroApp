3ed4ee247c97e75a7df87ad0048572cdf3d379cb
Samuel Lima <samuelimvu6@gmail.com>
2023-01-12 21:58:43 -0300

    first commit

---
+++ lib/components/entrada_tempo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+
+class EntradaTempo extends StatelessWidget {
+  final String titulo;
+  final int valor;
+  final void Function()? inc;
+  final void Function()? dec;
+
+  const EntradaTempo(
+      {required this.titulo, required this.valor, this.inc, this.dec, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+    return Column(
+      children: [
+        Text(titulo),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: dec,
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: store.estarTrabalhando() ? Colors.red : Colors.green,
+              ),
+              child: const Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '$valor min',
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: inc,
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                primary: store.estarTrabalhando() ? Colors.red : Colors.green,
+              ),
+              child: const Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/store/pomodoro.store.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+
+import 'package:mobx/mobx.dart';
+
+part 'pomodoro.store.g.dart';
+
+class PomodoroStore = _PomodoroStore with _$PomodoroStore;
+
+enum TipoIntervalo { trabalho, descanso }
+
+abstract class _PomodoroStore with Store {
+  @observable
+  bool iniciado = false;
+
+  @observable
+  int minutos = 2;
+
+  @observable
+  int segundos = 1;
+
+  @observable
+  int tempoTrabalho = 2;
+
+  @observable
+  int tempoDescanso = 1;
+
+  @observable
+  TipoIntervalo tipoIntervalo = TipoIntervalo.trabalho;
+
+  Timer? cronometro;
+
+  @action
+  void iniciar() {
+    iniciado = true;
+    cronometro = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (minutos == 0 && segundos == 0) {
+        _trocarIntervalo();
+      } else if (segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
+  }
+
+  @action
+  void parar() {
+    iniciado = false;
+    cronometro?.cancel();
+  }
+
+  void reiniciar() {
+    parar();
+    minutos = estarTrabalhando() ? tempoTrabalho : tempoDescanso;
+    segundos = 0;
+  }
+
+  @action
+  void incrementarTempoTrabalho() {
+    tempoTrabalho++;
+    if (estarTrabalhando()) {
+      reiniciar();
+    }
+  }
+
+  @action
+  void decrementarTempoTrabalho() {
+    if (tempoTrabalho > 1) {
+      tempoTrabalho--;
+      if (estarTrabalhando()) {
+        reiniciar();
+      }
+    }
+  }
+
+  @action
+  void incrementarTempoDescanso() {
+    tempoDescanso++;
+    if (estarDescansando()) {
+      reiniciar();
+    }
+  }
+
+  @action
+  void decrementarTempoDescanso() {
+    if (tempoDescanso > 1) {
+      tempoDescanso--;
+      if (estarDescansando()) {
+        reiniciar();
+      }
+    }
+  }
+
+  bool estarTrabalhando() {
+    return tipoIntervalo == TipoIntervalo.trabalho;
+  }
+
+  bool estarDescansando() {
+    return tipoIntervalo == TipoIntervalo.descanso;
+  }
+
+  void _trocarIntervalo() {
+    if (estarTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.descanso;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.trabalho;
+      minutos = tempoTrabalho;
+    }
+  }
+}
